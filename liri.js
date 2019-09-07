d53b99c71d155d6c12a4f31629340f4da52d6e1f
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-09-07 19:51:58 -0400

    liri function update

--- liri.js
+++ liri.js
@@ -4,3 +4,26 @@ require("dotenv").config();
 var keys = require("./keys.js");
 // Access personal key information:s
 var spotify = new Spotify(keys.spotify);
+
+function switchCase() {
+    // Switch case statements
+    switch (action) {
+  
+      case 'spotify-this-song':
+        grabSong();
+        break;
+  
+      case 'movie-this':
+        grabMovie();
+        break;
+  
+      case 'do-what-it-says':
+        grabReadme();
+        break;
+  
+        default:                            // This is used for if there is a missing ' break ' in any of the statements 
+        console.log("Something Broke");
+        break;
+  
+    }
+  };
