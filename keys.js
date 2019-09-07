904e5488fdaa31b9abb69f75d1840ba5a2915b04
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-09-07 16:39:43 -0400

    added Spotify secret, id (not personal secret / id yet)

--- keys.js
+++ keys.js
@@ -1,2 +1,8 @@
 // keys
 
+console.log('this is loaded');
+console.log(process.env.SPOTIFY_SECRET);
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  }
